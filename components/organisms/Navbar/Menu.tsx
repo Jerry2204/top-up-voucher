31d6c23ebee590dc2dfecd697633abed4953a4f6
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2022-03-07 22:00:43 +0700

    create organisms navbar

---
+++ components/organisms/Navbar/Menu.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Menu() {
+  return (
+    <li className="nav-item my-auto">
+      <a className="nav-link active" aria-current="page" href="/#">
+        Home
+      </a>
+    </li>
+  );
+}
