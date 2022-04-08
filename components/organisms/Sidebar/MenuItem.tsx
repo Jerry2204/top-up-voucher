62de46a99cae81d4aae204cfd8bd207a9d34f164
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2022-04-08 22:10:00 +0700

    finishing refactoring sidebar

--- components/organisms/Sidebar/MenuItem.tsx
+++ components/organisms/Sidebar/MenuItem.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import Image from 'next/image';
+import cx from 'classnames';
 
 interface MenuItemProps {
     title: string;
+    icon: string;
+    active?: boolean
 }
 
-export default function MenuItem(props: MenuItemProps) {
-  const { title } = props;
+export default function MenuItem(props: Partial<MenuItemProps>) {
+  const { title, icon, active } = props;
+  const classItem = cx({
+    'item': true,
+    'mb-30': true,
+    active
+  });
+
   return (
-    <div className="item active mb-30">
+    <div className={classItem}>
       <div className="me-3">
-        <Image src="/icon/ic-menu-overview.svg" width={25} height={25} />
+        <Image src={`/icon/${icon}.svg`} width={25} height={25} />
       </div>
       <p className="item-title m-0">
         <a href="/#" className="text-lg text-decoration-none">
