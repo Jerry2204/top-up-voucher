5c18de8ade0239f974d5ad73e49ea7045928296f
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2022-04-10 23:39:17 +0700

    integration table row

--- components/organisms/OverviewContent/TableRow.tsx
+++ components/organisms/OverviewContent/TableRow.tsx
@@ -1,21 +1,28 @@
-import Image from 'next/image';
-
+import cx from 'classnames';
 interface TableRowProps {
   title: string;
   price: number;
   category: string;
   item: number;
-  status: string;
+  status: 'Failed' | 'Success' | 'Pending';
+  image: string;
 }
 
 export default function TableRow(props: TableRowProps) {
-  const { title, category, price, status, item } = props;
+  const { title, category, price, status, item, image } = props;
+  const statusClass = cx({
+    'float-start icon-status': true,
+    pending: status === 'Pending',
+    success: status === 'Success',
+    failed: status === 'Failed',
+  });
+
   return (
     <tr className="align-middle">
       <th scope="row">
-        <Image
+        <img
           className="float-start me-3 mb-lg-0 mb-3"
-          src="/img/overview-1.png"
+          src={`/img/${image}.png`}
           width={80}
           height={60}
           alt="game-thumbnail"
@@ -23,26 +30,23 @@ export default function TableRow(props: TableRowProps) {
         <div className="game-title-header">
           <p className="game-title fw-medium text-start color-palette-1 m-0">
             {title}
-            Mobile Legends: The New Battle 2021
           </p>
           <p className="text-xs fw-normal text-start color-palette-2 m-0">
-            {category} Desktop
+            {category}
           </p>
         </div>
       </th>
       <td>
-        <p className="fw-medium color-palette-1 m-0">{item}200 Gold</p>
+        <p className="fw-medium color-palette-1 m-0">{item} Gold</p>
       </td>
       <td>
-        <p className="fw-medium text-start color-palette-1 m-0">
-          {price}Rp 290.000
-        </p>
+        <p className="fw-medium text-start color-palette-1 m-0">{price}</p>
       </td>
       <td>
         <div>
-          <span className="float-start icon-status pending"></span>
+          <span className={statusClass}></span>
           <p className="fw-medium text-start color-palette-1 m-0 position-relative">
-            {status}Pending
+            {status}
           </p>
         </div>
       </td>
--- components/organisms/OverviewContent/index.tsx
+++ components/organisms/OverviewContent/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Category from './Category';
+import TableRow from './TableRow';
 
 export default function OverviewContent() {
   return (
@@ -41,118 +42,38 @@ export default function OverviewContent() {
                 </tr>
               </thead>
               <tbody>
-                
-                <tr className="align-middle text-center">
-                  <th scope="row">
-                    <img
-                      className="float-start me-3 mb-lg-0 mb-3"
-                      src="/img/overview-2.png"
-                      width="80"
-                      height="60"
-                      alt=""
-                    />
-                    <div className="game-title-header">
-                      <p className="game-title fw-medium text-start color-palette-1 m-0">
-                        Call of Duty:Modern
-                      </p>
-                      <p className="text-xs fw-normal text-start color-palette-2 m-0">
-                        Desktop
-                      </p>
-                    </div>
-                  </th>
-                  <td>
-                    <p className="fw-medium text-start color-palette-1 m-0">
-                      550 Gold
-                    </p>
-                  </td>
-                  <td>
-                    <p className="fw-medium text-start color-palette-1 m-0">
-                      Rp 740.000
-                    </p>
-                  </td>
-                  <td>
-                    <div>
-                      <span className="float-start icon-status success"></span>
-                      <p className="fw-medium text-start color-palette-1 m-0 position-relative">
-                        Success
-                      </p>
-                    </div>
-                  </td>
-                </tr>
-                <tr className="align-middle text-center">
-                  <th scope="row">
-                    <img
-                      className="float-start me-3 mb-lg-0 mb-3"
-                      src="/img/overview-3.png"
-                      width="80"
-                      height="60"
-                      alt=""
-                    />
-                    <div className="game-title-header">
-                      <p className="game-title fw-medium text-start color-palette-1 m-0">
-                        Clash of Clans
-                      </p>
-                      <p className="text-xs fw-normal text-start color-palette-2 m-0">
-                        Mobile
-                      </p>
-                    </div>
-                  </th>
-                  <td>
-                    <p className="fw-medium text-start color-palette-1 m-0">
-                      100 Gold
-                    </p>
-                  </td>
-                  <td>
-                    <p className="fw-medium text-start color-palette-1 m-0">
-                      Rp 120.000
-                    </p>
-                  </td>
-                  <td>
-                    <div>
-                      <span className="float-start icon-status failed"></span>
-                      <p className="fw-medium text-start color-palette-1 m-0 position-relative">
-                        Failed
-                      </p>
-                    </div>
-                  </td>
-                </tr>
-                <tr className="align-middle text-center">
-                  <th scope="row">
-                    <img
-                      className="float-start me-3 mb-lg-0 mb-3"
-                      src="/img/overview-4.png"
-                      width="80"
-                      height="60"
-                      alt=""
-                    />
-                    <div className="game-title-header">
-                      <p className="game-title fw-medium text-start color-palette-1 m-0">
-                        The Royal Game
-                      </p>
-                      <p className="text-xs fw-normal text-start color-palette-2 m-0">
-                        Mobile
-                      </p>
-                    </div>
-                  </th>
-                  <td>
-                    <p className="fw-medium text-start color-palette-1 m-0">
-                      225 Gold
-                    </p>
-                  </td>
-                  <td>
-                    <p className="fw-medium text-start color-palette-1 m-0">
-                      Rp 200.000
-                    </p>
-                  </td>
-                  <td>
-                    <div>
-                      <span className="float-start icon-status pending"></span>
-                      <p className="fw-medium text-start color-palette-1 m-0 position-relative">
-                        Pending
-                      </p>
-                    </div>
-                  </td>
-                </tr>
+                <TableRow
+                  title="Mobile Legend"
+                  category="Mobile"
+                  item={200}
+                  price={290000}
+                  status="Pending"
+                  image="overview-1"
+                />
+                <TableRow
+                  title="Call of Duty"
+                  category="Desktop"
+                  item={500}
+                  price={180000}
+                  status="Success"
+                  image="overview-2"
+                />
+                <TableRow
+                  title="Clash of Clans"
+                  category="Mobile"
+                  item={100}
+                  price={200000}
+                  status="Success"
+                  image="overview-3"
+                />
+                <TableRow
+                  title="The Royal Game"
+                  category="Mobile"
+                  item={225}
+                  price={250000}
+                  status="Success"
+                  image="overview-4"
+                />
               </tbody>
             </table>
           </div>
