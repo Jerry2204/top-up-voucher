326e526791539d85d9c2f5e96ea24e797f7bb0f3
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2022-04-11 21:19:51 +0700

    fixing sidebar route

--- components/organisms/Sidebar/index.tsx
+++ components/organisms/Sidebar/index.tsx
@@ -3,17 +3,28 @@ import Footer from './Footer';
 import Profile from './Profile';
 import MenuItem from './MenuItem';
 
-export default function Sidebar() {
+interface SidebarProps {
+  activeMenu: 'overview' | 'transactions' | 'settings';
+}
+
+export default function Sidebar(props: SidebarProps) {
+  const { activeMenu } = props;
   return (
     <section className="sidebar">
       <div className="content pt-50 pb-30 ps-30">
         <Profile />
         <div className="menus">
-          <MenuItem title="Overview" icon="ic-menu-overview" href="/" active />
+          <MenuItem
+            title="Overview"
+            icon="ic-menu-overview"
+            href="/member"
+            active={activeMenu === 'overview'}
+          />
           <MenuItem
             title="Transactions"
             icon="ic-tote-bag"
             href="/member/transactions"
+            active={activeMenu === 'transactions'}
           />
           <MenuItem title="Messages" icon="ic-message" href="/member" />
           <MenuItem title="Card" icon="ic-card" href="/member" />
@@ -22,6 +33,7 @@ export default function Sidebar() {
             title="Settings"
             icon="ic-settings"
             href="/member/edit-profile"
+            active={activeMenu === 'settings'}
           />
           <MenuItem title="Log Out" icon="ic-shutdown" href="/sign-in" />
         </div>
